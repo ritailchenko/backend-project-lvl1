0859cc08dbb07e29c67a3e31e2ecb552f4f87a00
Rita Ilchenko <rita@Rita.local>
2020-09-05 18:41:26 -0400

    added readlineSync

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,3 +1,10 @@
 #!/usr/bin/env node
 
-console.log("  Welcome to the brain games!");
+console.log("Welcome to the brain games!");
+
+import readlineSync from "readline-sync";
+
+const name = readlineSync.question("what is your name?");
+// const name = readlineSync.question('Your answer: ');
+
+console.log(`Hi, ${name.toUpperCase()}, welcome to the game!`);
