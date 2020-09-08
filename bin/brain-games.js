f5b84faea896ed4bfc4d9c14c20d5f42164312f3
Rita Ilchenko <rita@Rita.local>
2020-09-08 09:51:10 -0400

    fixed prettier

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
-import readlineSync from "readline-sync";
-import greeting from "../src/cli.js";
+import readlineSync from 'readline-sync';
+import greeting from '../src/cli.js';
 
-console.log("Welcome to the brain games!");
+console.log('Welcome to the brain games!');
 
-const name = readlineSync.question("what is your name?");
+const name = readlineSync.question('what is your name?');
 greeting(name);
